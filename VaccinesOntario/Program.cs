d1f12a0b64f25ad65c2982013781d5299be29283
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-04-02 12:56:20 -0400

    Add project files.

---
+++ VaccinesOntario/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+
+namespace VaccinesOntario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read the lines from the file, seperated by newline
+            string[] lines = Properties.Resources.Vaccines.Split('\n');
+            Vaccine[] vaccines = new Vaccine[lines.Length];
+            //iterate through the file
+            foreach (string line in lines)
+            {
+                //initialize a counter and array of vaccines
+                int vacCounter = 0;
+                
+
+                Console.WriteLine(line);
+                  string[] columns = line.Split(',');
+                int counter = 0;
+               // Vaccine tempVaccine = new Vaccine();
+                 foreach (string column in columns)
+                 {
+                    if (column == "")
+                    {
+                        break;
+                    }
+                     Console.WriteLine(column);
+
+                    if (counter == 0)
+                    {
+                        //Add SKU Number
+                        vaccines[vacCounter].setSKU(int.Parse(column));
+                    }
+                    else if (counter == 1)
+                    {
+                        //Add Vaccine Name
+                        vaccines[vacCounter].setName(column);
+                    }
+                    else if (counter == 2)
+                    {
+                        //Add Unit Cost
+                        vaccines[vacCounter].setCost(float.Parse(column));
+                    }
+                    else if (counter == 3)
+                    {
+                        //Add Quantity
+                        vaccines[vacCounter].setQuantity(int.Parse(column));
+                    }
+                    else if (counter == 4)
+                    {
+                        //Add Expiry
+                        DateTime tempDate = new DateTime();
+                        if (DateTime.TryParseExact(column, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture, 
+                            System.Globalization.DateTimeStyles.None, out tempDate))
+                        {
+                            vaccines[vacCounter].setDate(tempDate);
+                        }
+                    }
+                    else if (counter == 5)
+                    {
+                        //Add Instructions
+                        vaccines[vacCounter].setInstructions(column);
+                    }
+                    counter++;
+                 }
+                
+                vacCounter++;
+            }
+
+            vaccines[0].ToString();
+            vaccines[1].ToString();
+            /*
+
+            bool showMenu = true;
+
+            while (showMenu)
+            { 
+            
+            Console.WriteLine("Welcome to the Vaccine Manager, Please Select an Option!");
+             showMenu = MainMenu();
+            }
+            
+            */
+        }
+
+        /// <summary>
+        /// Method to show the Main Menu and allow for a user choice
+        /// </summary>
+        /// <returns>bool</returns>
+        public static bool MainMenu()
+        {
+            Console.Clear();
+            //Display Main Menu
+            Console.Write("\n--------------------------------------------------------\n");
+            Console.WriteLine("1) List Vaccines\n");
+            Console.WriteLine("2) Display a Vaccine\n");
+            Console.WriteLine("3) Add Vaccine\n");
+            Console.WriteLine("4) Update Vaccine\n");
+            Console.WriteLine("5) Delete Product\n");
+            Console.WriteLine("6) Sort Products\n");
+            Console.WriteLine("0) Exit Program\n");
+            //Allow User Choice
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Option 1 Selected");
+                    Console.ReadKey();
+                    return true;
+                case "2":
+                    Console.WriteLine("Option 2 Selected");
+                    Console.ReadKey();
+                    return true;
+                case "3":
+                    Console.WriteLine("Option 3 Selected");
+                    Console.ReadKey();
+                    return true;
+                case "4":
+                    Console.WriteLine("Option 4 Selected");
+                    Console.ReadKey();
+                    return true;
+                case "5":
+                    Console.WriteLine("Option 5 Selected");
+                    Console.ReadKey();
+                    return true;
+                case "6":
+                    Console.WriteLine("Option 6 Selected");
+                    Console.ReadKey();
+                    return true;
+                case "0": //User Wants to close the program
+                    QuitMenu();
+                    return false;
+                default:
+                    Console.WriteLine("Please Select a Valid Option. 1 thru 6, or 0 to quit.");
+                    Console.ReadKey();
+                    return true;
+            }
+        }
+        /// <summary>
+        /// Closes the Application
+        /// </summary>
+        public static void QuitMenu()
+        {
+            Console.WriteLine("See you again! Press any key to exit...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+     //   public Vaccine ReadFile(string path) //should be string of path to resources file
+     //   {
+            
+      //  }
+
+    }//End of Main
+}//End of Namespace
---
+++ VaccinesOntario/Vaccine.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaccinesOntario
+{
+    class Vaccine
+    {
+        //Private Variables
+        private int SKU;
+        private string name;
+        private float cost;
+        private int quantity;
+        private DateTime expiration;
+        private string instructions;
+
+        //Constructors
+        //Default Constructor
+        public Vaccine()
+        {
+
+        }
+        //Parameterized Constructor
+        public Vaccine(int SKU, string name, float cost, int quantity, DateTime expiration, string instructions)
+        {
+            setSKU(SKU);
+            setName(name);
+            setCost(cost);
+            setQuantity(quantity);
+            setDate(expiration);
+            setInstructions(instructions);
+        }
+
+        //Getters and Setters
+        public int getSKU()
+        {
+            return SKU;
+        }
+
+        public void setSKU(int SKU)
+        {
+            this.SKU = SKU;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public float getCost()
+        {
+            return cost;
+        }
+
+        public void setCost(float cost)
+        {
+            this.cost = cost;
+        }
+
+        public int getQuantity()
+        {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity)
+        {
+            this.quantity = quantity;
+        }
+
+        public DateTime getDate()
+        {
+            return expiration.Date;
+        }
+
+        public void setDate(DateTime date)
+        {
+            expiration = date;
+        }
+
+        public string getInstructions()
+        {
+            return instructions;
+        }
+
+        public void setInstructions(string instruction)
+        {
+            this.instructions = instruction;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            string tempString = "";
+
+            tempString += "SKU: " + getSKU().ToString() + Environment.NewLine;
+            tempString += "Vaccine Name: " + getName() + Environment.NewLine;
+            tempString += "Unit Cost: " + getCost().ToString() + Environment.NewLine;
+            tempString += "Quantity on hand: " + getQuantity().ToString() + Environment.NewLine;
+            tempString += "Expiry Date: " + getDate().ToString() + Environment.NewLine;
+            tempString += "Special Instructions: " + getInstructions() + Environment.NewLine;
+
+            return tempString;
+        }
+    }
+}
